ab7df6014c3ecb7f3897cbb8e3f48fe265e41ba6
Brandon Curtis <Brandcurtis43@gmail.com>
2020-10-12 17:12:01 -0600

    Added more classes and started working on the add task function and such. Also changed some styling of highlighting active lists.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
 // Variables
 const listOfLists = document.getElementById('listOfLists')
 const allLists = new AllListContainer()
+let currentList
 
 // Functions
+function selectList(id) {
+    currentList = allLists.getList(id);
+    const node = document.getElementById(id);
+    [...document.getElementsByClassName('active')].forEach(element => {
+        element.classList.remove('active');
+    })
+    node.classList.add('active');
+    console.log(currentList);
+}
+
 function newList(event) {
     if (event.keyCode === 13) {
         const taskListName = event.target.value;
-        const newId = randomId();
-        allLists.addList(listOfLists, newId, taskListName)
+        allLists.addList(listOfLists, taskListName)
         event.target.value = '';
     }
 }
 
+function addTask(event) {
+    if (currentList !== undefined) {
+
+    }
+}
--- js/tasks.js
+++ js/tasks.js
@@ -2,13 +2,12 @@ class AllListContainer {
     constructor() {
         this.lists = {};
     }
-    addList(location, id, name) {
-        this.lists[id] = {
-            id,
-            name,
-        }
-        const node =`
-            <div id="${id}" class="h5 pb-4 pt-4 pl-5">
+    addList(location, name) {
+        const id = randomId();
+        this.lists[id] = new List(id, name);
+        const node =
+            `
+            <div id="${id}" class="h5 pb-4 pt-4 pl-5" onclick="selectList(${id})">
             <p>${name}</p>
             </div>
             `
@@ -19,5 +18,47 @@ class AllListContainer {
     }
     removeList(id) {
         this.lists[id] = null;
+    }
+}
+
+class Task {
+    constructor(id, name, description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.isComplete = false;
+    }
+    editTask(name, description, isComplete) {
+        this.name = name;
+        this.description = description;
+        this.isComplete = isComplete;
+    }
+}
+
+class List {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.tasks = {};
+    }
+    addTask(name, description) {
+        const id = randomId();
+        this.tasks[id] = new Task(id, name, description);
+        this.renderTasks();
+    }
+    renderTasks() {
+        document.getElementById('taskArea').innerHTML = '';
+        let allTasks = '';
+        for (const id in this.tasks) {
+            const task = this.tasks[id];
+            allTasks += 
+                `
+                <div id="${id}" class="taskBorder">
+                <div>${task.name}</div>
+                <div>${task.description}</div>
+                </div>
+                `
+        }
+        document.getElementById('taskArea').insertAdjacentHTML('beforeend', allTasks);
     }
 }
